ae6182d99d0c5cd5eac99eff06606fda7387f34e
Hector Emanuel Peña Cuevas <emanuel.cuevas03@gmail.com>
2024-07-17 23:00:06 +0200

    feat: main.ts MS config &&  nats server && docker config

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,8 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { NATS_SERVICE } from 'src/config';
+
+@Injectable()
+export class AuthService {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const logger = new Logger('Auth Microservice');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+  );
+  await app.listen();
+  logger.log(`Auth Microservice is up and running`);
 }
 bootstrap();
